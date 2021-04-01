538d51113902b26d56b5f73e8f87ffe8ee6da756
nikkoli castilan <ndcastilan@gmail.com>
2021-04-01 20:57:22 +1300

    todo

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,48 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateTodo = /* GraphQL */ `
+  subscription OnCreateTodo {
+    onCreateTodo {
+      id
+      name
+      IdentificationNumber
+      SeatNumber
+      ContactNumber
+      Attendance
+      Timedmitted
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateTodo = /* GraphQL */ `
+  subscription OnUpdateTodo {
+    onUpdateTodo {
+      id
+      name
+      IdentificationNumber
+      SeatNumber
+      ContactNumber
+      Attendance
+      Timedmitted
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteTodo = /* GraphQL */ `
+  subscription OnDeleteTodo {
+    onDeleteTodo {
+      id
+      name
+      IdentificationNumber
+      SeatNumber
+      ContactNumber
+      Attendance
+      Timedmitted
+      createdAt
+      updatedAt
+    }
+  }
+`;
