a4babe70e138b69a8ffffcb92717574175cc53b7
nikkoli castilan <ndcastilan@gmail.com>
2021-04-01 20:00:18 +1300

    test

--- src/App.js
+++ src/App.js
@@ -1,49 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import { EasybaseProvider, useEasybase } from 'easybase-react';
-import { useEffect } from 'react';
-import ebconfig from './ebconfig';
 
 function App() {
   return (
-    <div className="App" style={{ display: "flex", justifyContent: "center" }}>
-     <EasybaseProvider ebconfig={ebconfig}>
-        <Notes />
-      </EasybaseProvider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hello from V2</h1>
+      </header>
     </div>
   );
-}
-function Notes() {
-  const { Frame, sync, configureFrame } = useEasybase();
-
-  useEffect(() => {
-    configureFrame({ tableName: "TEST", limit: 10 });
-    sync();
-  }, []);
-
-  const noteRootStyle = {
-    border: "2px #0af solid",
-    borderRadius: 9,
-    margin: 20,
-    backgroundColor: "#efefef",
-    padding: 6
-  };
-
-  return (
-    <div style={{ width: 400 }}>
-
-        <div style={noteRootStyle}>
-          <ul>
-  {Frame().map(item =>
-     <li key = {item.title}>{item.title}</li>
-      )}
-    </ul>
-        </div>
-
-
-
-
-    </div>
-  )
 }
 
 export default App;
